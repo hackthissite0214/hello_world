c27a82f07ebfc40dd6f44cda522bc2f1db895a9c
csi <robin256@naver.com>
2020-09-14 13:06:28 +0900

    Hello_World2

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = 3000;
 app.listen(port, () => {
